d294d7e478769d618f1dd274452b88b8a107fcc0
benjie <benjie.work@gmail.com>
2020-02-23 02:22:39 +0800

    Implemented a JavaScript for loop on home.ejs to loop through the title of each post and console.log() them

--- app.js
+++ app.js
@@ -18,8 +18,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-  res.render("home", {startingContent: homeStartingContent});
-  console.log(posts);
+  res.render("home", {
+    startingContent: homeStartingContent,
+    blogPost: posts
+  });
 });
 
 app.get("/about", function(req, res){
