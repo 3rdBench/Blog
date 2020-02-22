63815849bcc8eca5be5d1369beec6b4f75e95610
benjie <benjie.work@gmail.com>
2020-02-23 02:32:33 +0800

    Implemented Express Routing Parameters in app.js; console logs the blog entry's post name when typed in the URL (http://localhost:3000/posts/<post_name>)

--- app.js
+++ app.js
@@ -44,7 +44,10 @@ app.post("/compose", function(req, res){
 
   posts.push(post);
   res.redirect("/");
+});
 
+app.get("/posts/:postName", function(req, res){
+  console.log(req.params.postName);
 });
 
 app.listen(3000, function() {
