e734762b2136311549edb41b393e794bc9ce2ce0
benjie <benjie.work@gmail.com>
2020-02-23 01:58:17 +0800

    Implemented a JavaScript object named post to store user-submitted blog title and post (i.e. content)

--- app.js
+++ app.js
@@ -32,7 +32,11 @@ app.get("/compose", function(req, res){
 });
 
 app.post("/compose", function(req, res){
-  console.log(req.body.subjectItem);
+  const post = {
+    title: req.body.blogTitle,
+    content: req.body.blogPost
+  };
+  
 });
 
 app.listen(3000, function() {
